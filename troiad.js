0ef06a80eb79f8db35a7a881223901d77a9c8f28
Eric <73071377+Posoroko@users.noreply.github.com>
2024-02-02 08:30:28 -0500

    troiad repaired

--- troiad.js
+++ troiad.js
@@ -1,4 +1,4 @@
-const agendaURL = 'https://krismenn.monsieuredgar.com/items/Agenda?fields=id,date,city.name,showTemp&sort=date';
+const agendaURL = 'https://krismenn.monsieuredgar.com/items/Agenda?fields=id,date,city.translations.name,show.translations.title&sort=date';
 const agendaBox_div = document.getElementById('agendaBox');
 
 
@@ -23,11 +23,11 @@ const createEventBox = (event) => {
         wrapperBox_div.classList.add('wrapperBox');
             const showName_div = document.createElement('div');
             showName_div.classList.add('showName', 'dateInfo', 'spectacle');
-            showName_div.innerText = event.showTemp;
+            showName_div.innerText = event.show.translations[0].title;
         wrapperBox_div.appendChild(showName_div);
             const ville_div = document.createElement('div');
             ville_div.classList.add('ville', 'dateInfo');
-            ville_div.innerText = event.city.name;
+            ville_div.innerText = event.city.translations[0].name;
         wrapperBox_div.appendChild(ville_div);
     eventBox_div.appendChild(wrapperBox_div);
     agendaBox_div.appendChild(eventBox_div);
